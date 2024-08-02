cf601ec6a22001ad3a65dedc1d3c8e6c204ae2be
Uiciniv <v.santos111@outlook.com>
2024-08-01 22:23:08 -0300

    Limpeza dos arquivos CSS e criação do primeiro componente

---
+++ PWIII/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+import CardBooks from './components/CardBooks'
+
+function App() {
+
+  return (
+    <>
+      <CardBooks
+        titulo = "As Cavernas de Aço"
+        autor = "Isaac Asimov"
+      />
+
+      <CardBooks
+        titulo = "Dragon Ball"
+        autor = "Akira Toriyama"
+      />
+
+      <CardBooks
+        titulo = "One Piece"
+        autor = "Eiichiro Oda"
+      />
+
+<CardBooks
+        titulo = "Naruto"
+        autor = "Masashi Kishimoto"
+      />
+
+    </>
+  )
+}
+
+export default App
---
+++ PWIII/src/components/CardBooks.jsx
@@ -0,0 +1,14 @@
+const CardBooks = ({titulo,autor})=>{
+
+    return(
+        <div>
+            <h3>{titulo}</h3>
+            <p> {autor}</p>
+        </div>
+    )
+
+}
+
+export default CardBooks
+
+    
