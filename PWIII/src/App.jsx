a53c4cf391e85d3383e6fc5bd9a227625ed6bb5a
Uiciniv <v.santos111@outlook.com>
2024-08-22 22:09:09 -0300

    Criação de NavBar

--- PWIII/src/App.jsx
+++ PWIII/src/App.jsx
@@ -2,17 +2,16 @@ import './App.css'
 
 import CardBooks from './components/CardBooks'
 
+import NavBar from './components/layout/Navbar'
+
 import capaLivro from './assets/livros/livros/cavernas_aco.jpg'
 
 function App() {
 
   return (
     <>
-      <CardBooks
-        titulo = "As Cavernas de Aço"
-        autor = "Isaac Asimov"
-        imagem = {capaLivro}
-      />
+      
+      <NavBar/>
 
     </>
   )
